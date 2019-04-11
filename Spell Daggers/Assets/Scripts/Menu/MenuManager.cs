2f1dce03cf701c29fcd11504e6e34ee10d036af9
maxbrogno <mgbrogno@gmail.com>
2019-04-11 13:25:01 +1000

    Updated Menu scripts and scenes.

--- Spell Daggers/Assets/Scripts/Menu/MenuManager.cs
+++ Spell Daggers/Assets/Scripts/Menu/MenuManager.cs
@@ -9,16 +9,66 @@ public class MenuManager : MonoBehaviour {
     public Button playButton;
     public Button exitButton;
 
-	void Start () {
+    public GameObject menuPanel;
+    public GameObject difficultyPanel;
+
+    public Button easyButton;
+    public Button mediumButton;
+    public Button hardButton;
+
+    public Button backButton;
+    bool isShowingDifficulty;
+
+    void Start () {
         playButton.GetComponent<Button>().onClick.AddListener(PressPlay);
         exitButton.GetComponent<Button>().onClick.AddListener(PressExit);
+
+        easyButton.GetComponent<Button>().onClick.AddListener(PressEasy);
+        mediumButton.GetComponent<Button>().onClick.AddListener(PressMedium);
+        hardButton.GetComponent<Button>().onClick.AddListener(PressHard);
+
+        backButton.GetComponent<Button>().onClick.AddListener(PressBack);
+
+        difficultyPanel.SetActive(false);
+    }
+
+    void Update() {
+        if (isShowingDifficulty) {
+            menuPanel.SetActive(false);
+            difficultyPanel.SetActive(true);
+        } else {
+            menuPanel.SetActive(true);
+            difficultyPanel.SetActive(false);
+        }
     }
 
     void PressPlay() {
-        SceneManager.LoadScene(1);
+        Debug.Log("Play");
+        isShowingDifficulty = true;
     }
 
     void PressExit() {
+        Debug.Log("1");
         Application.Quit();
+    }
+
+    void PressEasy() {
+        Debug.Log("2");
+        SceneManager.LoadScene(1);
+    }
+
+    void PressMedium() {
+        Debug.Log("3");
+        SceneManager.LoadScene(1);
+    }
+
+    void PressHard() {
+        Debug.Log("4");
+        SceneManager.LoadScene(1);
+    }
+
+    void PressBack() {
+        Debug.Log("5");
+        isShowingDifficulty = false;
     }
 }
