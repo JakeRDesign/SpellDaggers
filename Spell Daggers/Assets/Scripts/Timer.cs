6996e4cfca35dc1aaa814e9da11a8e450e34448e
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2019-04-11 16:46:15 +1000

    updated the timer script to track player lives and pass the score to player prefs.

--- Spell Daggers/Assets/Scripts/Timer.cs
+++ Spell Daggers/Assets/Scripts/Timer.cs
@@ -5,26 +5,46 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
+    [Header("Timer")]
     public bool canTime = false;                        // bool that determines wherther the timer can run or not
     public Text timer;                                  // stores the UI element for the timer that will tick up
     public float timePassed;                            // how much time has passed
     public float speed;                                 // speed multiplier for the timer, for testing purposes
 
+    [Header("Health and End State")]
+    public int health = 3;                              // how many lives the players have
+
     private void Start()
     {
-        canTime = true;
+        canTime = true;                                 // starts the timer
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountTime();
+        EndState();
+    }
+
+    // if you can time, update the timer
+    void CountTime()
+    {
         if (canTime == true)
         {
-            timePassed += Time.deltaTime* speed;
+            timePassed += Time.deltaTime * speed;
             //string hours = (timePassed % 3600).ToString("00");
-            string minutes = Mathf.Floor((timePassed % 3600)/60).ToString("00");
+            string minutes = Mathf.Floor((timePassed % 3600) / 60).ToString("00");
             string seconds = (timePassed % 60).ToString("00");
             timer.text = /*hours + ":" + */minutes + ":" + seconds;
+        }
+    }
+
+    void EndState()
+    {
+        if (health <= 0)
+        {
+            canTime = false;
+            PlayerPrefs.SetFloat("score", timePassed);
         }
     }
 }
