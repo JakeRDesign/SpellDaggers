6b7371bf8a3a4d163ff8c09d7801a7b9ec6ec1e2
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2019-04-12 11:39:25 +1000

    Created a tutorial panel.

---
+++ Spell Daggers/Assets/Scripts/BulletKillZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKillZone : MonoBehaviour {
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Red" || other.tag == "Blue")
+        {
+            Destroy(other);
+        }
+    }
+}
