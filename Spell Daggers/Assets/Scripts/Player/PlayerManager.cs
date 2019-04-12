54293be77549b230eb352b7f07a7f0a33c2d8d10
ThePyromage <the.pyromagi@gmail.com>
2019-04-12 14:29:15 +1000

    Collision management removed from player manager

--- Spell Daggers/Assets/Scripts/Player/PlayerManager.cs
+++ Spell Daggers/Assets/Scripts/Player/PlayerManager.cs
@@ -28,13 +28,4 @@ public class PlayerManager : MonoBehaviour
 
 		transform.Rotate(Vector3.forward, m_movement * m_rotationSpeed * Time.deltaTime);
 	}
-
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		//swap enemy1 and enemy2 with whatever the enemy tags end up being
-		if (collision.gameObject.tag == "Enemy")
-		{
-			Timer.Instance.TakeDamage();
-		}
-	}
 }
