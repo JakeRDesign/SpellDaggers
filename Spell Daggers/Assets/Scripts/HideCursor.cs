6ea6e8573f7173dd07a2b1d5fe72500b9c9d92a0
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2019-04-12 15:39:55 +1000

    Icon, cursor fix. Final version.

---
+++ Spell Daggers/Assets/Scripts/HideCursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour {
+
+    public Canvas pause;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+        }
+
+        if (pause.enabled == false)
+        {
+            Cursor.visible = false;
+        }
+	}
+}
